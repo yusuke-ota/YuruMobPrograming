b07db4f039fe7c03dfeecd80077541b84cf86209
yusuke-ota <33338085+yusuke-ota@users.noreply.github.com>
2021-03-21 16:02:15 +0900

    キーボード入力の押しっぱなしが認識されない問題の解決
    SampleShooterControls.SetCallbacksからFixedUpdate内でポーリングする方式に変更
    SampleShooterControls.SetCallbacksで設定したイベントは値が変化したときのみ呼ばれるため、キーボード押しっぱなしの時は1回しか呼ばれない。
    XBOXコントローラは値が細かく変わるので引っかからなかったと考えられる。

--- Assets/Scenes/SampleShooting/PlayerController.cs
+++ Assets/Scenes/SampleShooting/PlayerController.cs
@@ -4,42 +4,44 @@ using UnityEngine.InputSystem;
 
 namespace Scenes.SampleShooting
 {
-    public class PlayerController : MonoBehaviour, SampleShooterControls.IShootingActions, IDamageable
+    public class PlayerController : MonoBehaviour, IDamageable
     {
         private SampleShooterControls _shooterControls;
-        private void Awake()
-        {
-            _shooterControls = new SampleShooterControls();
-            _shooterControls.Shooting.SetCallbacks(this);
-        }
-
-        private ObjectPool.ObjectPool _objectPool;
-        private uint _bulletLimit;
+        private void Awake() => _shooterControls = new SampleShooterControls();
         private void OnEnable() => _shooterControls.Shooting.Enable();
         private void OnDisable() => _shooterControls.Shooting.Disable();
-
-        #region IShootingActions実装部分
-
+        
+        private uint _bulletLimit;
+        private ObjectPool.ObjectPool _objectPool;
         [SerializeField, Tooltip("速度の係数")] private float speedCoefficient = 0.2f;
-        public void OnMove(InputAction.CallbackContext context)
-        {
-            var inputAxis = context.ReadValue<Vector2>();
-            // 入力が弱い場合(触っていない時など)は動作させない
-            if (inputAxis.sqrMagnitude < 0.01) return;
-
-            transform.localPosition += new Vector3(inputAxis.x, 0, inputAxis.y) * speedCoefficient;
-        }
-
-        public void OnShoot(InputAction.CallbackContext context)
+        private void FixedUpdate()
         {
             if (_objectPool is null)
             {
                 _objectPool = BulletManager.Instance.PlayerBulletPool;
                 _bulletLimit = BulletManager.Instance.BulletLimit;
             }
-            var buttonPush = context.ReadValue<float>();
-            // 入力が弱い場合(触っていない時など)は動作させない
-            if (buttonPush < 0.01) return;
+
+            var moveDistance = _shooterControls.Shooting.Move.ReadValue<Vector2>();
+            if (moveDistance.sqrMagnitude >= 0.01f)
+            {
+                OnMove(moveDistance);
+            }
+
+            var shootKeyInput = _shooterControls.Shooting.Shoot.ReadValue<float>();
+            if (shootKeyInput >= InputSystem.settings.defaultButtonPressPoint)
+            {
+                OnShoot();
+            }
+        }
+
+        private void OnMove(Vector2 moveDistance)
+        {
+            transform.localPosition += new Vector3(moveDistance.x, 0, moveDistance.y) * speedCoefficient;
+        }
+
+        private void OnShoot()
+        {
             if (_bulletLimit <= _objectPool.CountActiveObject()) return;
 
             var bullet = _objectPool.Rent();
@@ -47,8 +49,6 @@ namespace Scenes.SampleShooting
             bullet.transform.position = thisTransform.position;
             bullet.transform.rotation = thisTransform.rotation;
         }
-
-        #endregion
 
         [SerializeField] private uint life;
         public void Damage(){
