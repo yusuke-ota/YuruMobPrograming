acec3b00c0c36992e694175268611b4942e337e6
yusuke-ota <33338085+yusuke-ota@users.noreply.github.com>
2021-03-28 13:45:52 +0900

    DIコンテナ導入

---
+++ Assets/Scenes/SampleShooting/SampleSceneGameLifetimeScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Scenes.SampleShooting
+{
+    public class SampleSceneGameLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
