3ea98eef132fd1966f0486c26d1097c8f413d760
yusuke-ota <33338085+yusuke-ota@users.noreply.github.com>
2021-05-29 22:54:05 +0900

    ServiceLocator実装

--- Assets/Document/DIContainer/4.ServiceLocator/ServiceLocator.cs
+++ Assets/Document/DIContainer/4.ServiceLocator/ServiceLocator.cs
@@ -1,11 +1,12 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
 
-namespace Document.DebugLog
+namespace Document.DIContainer._4.ServiceLocator
 {
     public static class ServiceLocator
     {
         // 型とインスタンスを対応付ける。
-        private var typeInstancePair = new Dictionary<Type, object>;
+        private static readonly Dictionary<Type, object> TypeInstancePair = new Dictionary<Type, object>();
 
         /// <summary>
         /// 型と具象オブジェクトを対応付ける。
@@ -18,25 +19,26 @@ namespace Document.DebugLog
         {
 			// typeとinstanceは1対1である必要があるため、すでにあった場合は入れ替える処理を行う。
 			// エラーを出しても良い。
-            if (typeInstancePair.Containe(type))
+            if (TypeInstancePair.ContainsKey(type))
             {
-                typeInstancePair[type] = instance;
+                TypeInstancePair[type] = instance;
             }
             else
             {
-                typeInstancePair.Add(type, instance);
+                TypeInstancePair.Add(type, instance);
             }
         }
 
         // TType: 適当に名前をつけたジェネリック型です。名前に特に意味はありません。
         /// <summary>
         /// 登録しておいた型とオブジェクトの対応テーブルを用いて、型情報から生成済みのオブジェクトを受け取る。
+        /// objectから目的の型へのキャストがあるので、ちょっと速度が落ちる。(機械側からは安全にキャストできるか分からないので)
         /// </summary>
-        /// <param name="type">欲しいオブジェクトの型(インターフェース)</param>
+        /// <param name="type">登録済みの欲しいオブジェクトの型(インターフェース)</param>
         /// <returns>型(インターフェース)に対応する具象クラスのオブジェクト</returns>
-        public static TType Resolve(TType type)
+        public static TType Resolve<TType>(TType type)
         {
-            return typeInstancePair[tyoeof(type)];
+            return (TType)TypeInstancePair[typeof(TType)];
         }
     }
 }
